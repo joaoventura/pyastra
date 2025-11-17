ea8ad73ef89334385c2ffd5ad3971ebc0bffd09f
joaoventura <joaojonesventura@gmail.com>
2025-11-17 23:08:50 +0000

    Update returns.py

--- pyastra/predictives/returns.py
+++ pyastra/predictives/returns.py
@@ -3,6 +3,7 @@ This module provides useful functions for handling solar and lunar returns.
 It only handles solar returns for now.
     
 """
+import dataclasses
 
 from pyastra import const
 from pyastra.ephem import ephem
@@ -14,21 +15,22 @@ def _compute_chart(chart, date):
     Internal function to return a new chart for a specific date using properties from old chart.
     
     """
-    pos = chart.pos
-    hsys = chart.hsys
+    context = dataclasses.replace(chart.context, jd=date.jd)
     ids = [obj.id for obj in chart.objects]
-    return Chart(date, pos, IDs=ids, hsys=hsys)
+    return Chart.from_context(context, ids)
 
 
 def next_solar_return(chart, date):
     """ Returns the solar return of a Chart after a specific date. """
     sun = chart.get_object(const.SUN)
-    sr_date = ephem.next_solar_return(date, sun.lon)
+    context = dataclasses.replace(chart.context, jd=date.jd)
+    sr_date = ephem.next_solar_return(sun.lon, context)
     return _compute_chart(chart, sr_date)
 
 
 def prev_solar_return(chart, date):
     """ Returns the solar return of a Chart before a specific date. """
     sun = chart.get_object(const.SUN)
-    sr_date = ephem.prev_solar_return(date, sun.lon)
+    context = dataclasses.replace(chart.context, jd=date.jd)
+    sr_date = ephem.prev_solar_return(sun.lon, context)
     return _compute_chart(chart, sr_date)
